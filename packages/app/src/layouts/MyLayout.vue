77db70a284c09d84e4a202f98f13d2cf5822fee3
Jerome Mare <jerome.mare@inetum.com>
2023-08-23 21:32:03 +0200

    feat: mise en place du projet de drawer

--- packages/app/src/layouts/MyLayout.vue
+++ packages/app/src/layouts/MyLayout.vue
@@ -2,13 +2,99 @@
   <q-layout view="hHr Lpr fFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn flat round dense icon="menu" />
+        <q-btn flat round dense icon="menu" @click="drawer=!drawer" />
         <q-toolbar-title>
           <span class="app-title">{{ appTitle }}</span>
         </q-toolbar-title>
         <q-btn flat round dense icon="search" @click="openSearch" />
       </q-toolbar>
     </q-header>
+
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="300"
+        :breakpoint="400"
+        overlay
+      >
+        <q-scroll-area style="height: calc(100% - 80px); margin-top: 80px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item-label header>Abonnements</q-item-label>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="add" />
+              </q-item-section>
+
+              <q-item-section>
+                Ajouter
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="mdi-eye" />
+              </q-item-section>
+
+              <q-item-section>
+                Visualiser
+              </q-item-section>
+            </q-item>
+
+            <q-separator />
+            <q-item-label header>Documents</q-item-label>
+
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="search" />
+              </q-item-section>
+
+              <q-item-section>
+                Rechercher
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="book" />
+              </q-item-section>
+
+              <q-item-section>
+                Emprunter
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="book" />
+              </q-item-section>
+
+              <q-item-section>
+                Renouveller
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="lock" />
+              </q-item-section>
+
+              <q-item-section>
+                Réserver
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="/img/fond-byblios-5.jpg" style="height: 80px">
+          <div class="row bg-transparent">
+            <q-img src="/img/byblios-avatar.png" width="56px" />
+            <div class="column" style="padding-top: 10px; padding-left: 15px;">
+              <div class="text-weight-bold app-title">{{ appTitle }}</div>
+              <div>Bibliothèque de Toulouse</div>
+            </div>
+          </div>
+        </q-img>
+      </q-drawer>
 
     <q-page-container>
       <div class="wrapper">
@@ -78,6 +164,7 @@ export default {
     return {
       tab: "",
       isSearchOpened: false,
+      drawer: false,
     };
   },
   computed: {
